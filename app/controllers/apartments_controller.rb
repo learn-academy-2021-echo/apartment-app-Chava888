a882a00f17d4d1de0e69212d9fb32c0d59fbabf8
Chava888 <salvador_tena@hotmail.com>
2022-02-10 16:01:50 -0800

    working on authorization

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,6 +1,12 @@
 class ApartmentsController < ApplicationController
     def index
+        if user_signed_in?
         apartments = Apartment.all
         render json: apartments
+       else
+        render 'index.html.erb'
+       end
+        
+ 
     end
 end
